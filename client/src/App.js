4bb5b3a5d35cf557aa45960c599d68b35fee98a8
Warnerhannah <warnerhannah1@yahoo.com>
2019-04-23 11:02:11 -0700

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route
+            path="/"
+            exact 
+            component={Books}
+          />
+          <Route
+            path="/books"
+            exact
+            component={Books}
+          />
+          <Route 
+            exact path="/books/:id"
+            component={Detail}
+          />
+          <Route
+            component={NoMatch}
+          />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+
+export default App;
